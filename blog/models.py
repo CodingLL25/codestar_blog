ef81871c040e9eab150a0feb62c37470c576d701
CodingLL25 <6b45ofgn@students.codeinstitute.net>
2025-10-21 19:37:48 +0100

    Amend code to try fix issue with django summernote

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -13,14 +13,15 @@ class Post(models.Model):
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return f"'{self.title}' | written by {self.author}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
